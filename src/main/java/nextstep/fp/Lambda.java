ab400a095930f90adfc81330218976119bbb26a8
scoopsoup <rnrdud46@nate.com>
2023-04-11 21:47:10 +0900

    feat : lambda 형식으로 변경하기

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -14,43 +14,45 @@ public class Lambda {
     public static void printAllLambda(List<Integer> numbers) {
         System.out.println("printAllLambda");
 
+        //numbers.forEach((Integer number) -> System.out.println(number));
         numbers.forEach(System.out::println);
     }
 
     public static void runThread() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello from thread");
-            }
-        }).start();
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("Hello from thread");
+//            }
+//        }).start();
+        new Thread(() -> System.out.println("Hello from thread")).start();
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
+        int total = numbers.stream().mapToInt(number -> number).sum();
+//        for (int number : numbers) {
+//            total += number;
+//        }
         return total;
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
+        int total = numbers.stream().filter(number -> number % 2 == 0).mapToInt(number -> number).sum();
+//        for (int number : numbers) {
+//            if (number % 2 == 0) {
+//                total += number;
+//            }
+//        }
         return total;
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
-                total += number;
-            }
-        }
+        int total = numbers.stream().filter(number -> number > 3).mapToInt(number -> number).sum();
+//        for (int number : numbers) {
+//            if (number > 3) {
+//                total += number;
+//            }
+//        }
         return total;
     }
 }
